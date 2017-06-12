3dc70dd2856c2c9652f937f5e12da52d2c988e21
Avinash Ranjan <avinash.ranjan21@gmail.com>
2017-06-13 00:50:59 +0530

    two button added for start and stop service

--- app/src/main/java/com/devschool/androidservicedemo/MainActivity.java
+++ app/src/main/java/com/devschool/androidservicedemo/MainActivity.java
@@ -1,13 +1,35 @@
 package com.devschool.androidservicedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_startService)
+    Button btnStartService;
+    @BindView(R.id.btn_stopService)
+    Button btnStopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_startService, R.id.btn_stopService})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_startService:
+                break;
+            case R.id.btn_stopService:
+                break;
+        }
     }
 }
